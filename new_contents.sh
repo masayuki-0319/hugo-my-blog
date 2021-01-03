32074d7e871e85ab96c51f7ff9680ae5d812ced3
Masayuki Onishi <omccello@gmail.com>
2021-01-03 16:59:42 +0900

    新記事生成用のshell修正

--- new_contents.sh
+++ new_contents.sh
@@ -1,15 +1,23 @@
 #!/bin/bash -eu
 
 if [[ $# -lt 2 ]] ; then
-  echo "第一引数として「記事のタイプ」、第二引数として「記事の名称」を入力すること"
+  echo "第一引数として「記事の種類」、第二引数として「記事の名称」を入力すること"
   exit 1
 fi
 
-CONTENT_TYPE=$1
-NEW_FILE_NAME=$2
+content_type=$1
+new_file_name="$2.md"
 
-hugo new "${CONTENT_TYPE}/${NEW_FILE_NAME}"
+ALLOW_CONTENT_TYPE=("posts" "memo")
+if ! `echo ${ALLOW_CONTENT_TYPE[@]} | grep -qE "( |^)${content_type}( |$)"` ; then
+    echo "記事の種類は [${ALLOW_CONTENT_TYPE[@]}] のいずれかを入力すること"
+    exit 1
+fi
 
-open "content/${CONTENT_TYPE}/${NEW_FILE_NAME}" -a /Applications/Visual\ Studio\ Code.app
+create_date=`date +"%Y%m%d"`
+file_path="${content_type}/${create_date}-${new_file_name}"
+
+hugo new $file_path
+open "content/${file_path}" -a /Applications/Visual\ Studio\ Code.app
 
 exit 0
